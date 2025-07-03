2e639f7ef88b42cc9251bdeaa004326776962968
varghesegt <varghese22605@gmail.com>
2025-07-04 00:30:07 +0530

    Fix: Blank page issue - proper react-head usage

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// If you're using `react-head`
+import { HeadProvider } from 'react-head';
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <HeadProvider>
     <App />
-  </React.StrictMode>
+  </HeadProvider>
 );
