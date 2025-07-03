bbe7ba92670dec0c9fbd0a07fc5dcfb89c2b57d6
varghesegt <varghese22605@gmail.com>
2025-07-04 00:21:52 +0530

    Fix: removed react-helmet-async from index.js

--- src/index.js
+++ src/index.js
@@ -3,12 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-import { HelmetProvider } from 'react-helmet-async';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <HelmetProvider>
+  <React.StrictMode>
     <App />
-  </HelmetProvider>
+  </React.StrictMode>
 );
